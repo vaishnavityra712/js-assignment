d83a4fa77c134a27163df5fa9167a6d03e4a36e2
VAISHNAVI ACHANTA <53216430+vaishnavityra712@users.noreply.github.com>
2020-07-19 23:56:51 +0530

    Add files via upload

---
+++ DAY 6 assignment 2.js
@@ -0,0 +1,11 @@
+const student = {
+    name : "Helsink",
+    age : 24,
+    project :{
+        diceGame : "The player dice game using javascript."
+    },
+}
+console.log(student.name);
+console.log(student.age);
+console.log(student.project);
+console.log(student.project.diceGame);
