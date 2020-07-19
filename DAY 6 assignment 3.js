192500029b34c1a6a1b1b17a983d896bfc81d326
VAISHNAVI ACHANTA <53216430+vaishnavityra712@users.noreply.github.com>
2020-07-19 23:57:36 +0530

    Add files via upload

---
+++ DAY 6 assignment 3.js
@@ -0,0 +1,5 @@
+let shoppinglist = ['tomatoes','radishes','carrots','bringal'];
+let shoppingBucket = [...shoppinglist];
+shoppingBucket.push('ladiesfinger','broccoli');
+console.log(shoppingBucket);
+console.log(shoppinglist);
