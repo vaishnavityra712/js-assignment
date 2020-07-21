dcccc69399df08f4e44f06cd463d1b0ccdda1435
VAISHNAVI ACHANTA <53216430+vaishnavityra712@users.noreply.github.com>
2020-07-21 12:39:30 +0530

    Add files via upload

---
+++ DAY 7 assignment 1-2.js
@@ -0,0 +1,8 @@
+//Makes An array of numbers till the number given by the user.
+let array = [];
+let n = Number(prompt("enter a number"));
+for (let i = 0; i<= n; i++) {
+    array.push(i);
+    
+}
+console.log(array);
